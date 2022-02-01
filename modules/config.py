385f5be9b8a4dc07e78afe2090cf90fe853f299f
Natasha Dudek <62709661+natasha-dudek@users.noreply.github.com>
2022-02-01 13:34:04 -0500

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,7 @@
+DATA_ROOT = "/Users/natasha/Documents/wine_label_generator/"
+SAQ_DATA = DATA_ROOT+"SAQ_wine_selection_wPrice.txt"
+WE_DATA = DATA_ROOT+"winemag-data-130k-v2.csv"
+GLOVE = DATA_ROOT+"glove_embeddings/glove.6B.300d.txt"
+
+MODEL = DATA_ROOT+"2022-1-17-9-18-51_lr0.001_e5_bs64_gn5_drop0.05_size300_embed300_seq50min10_k5_wine_generator_model.pt"
+LOSSES = DATA_ROOT+"2022-1-17-9-18-51_lr0.001_e5_bs64_gn5_drop0.05_size300_embed300_seq50min10_k5_wine_generator_train_losses.pt"
